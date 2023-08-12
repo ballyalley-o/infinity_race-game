1c3fbc256ddb00ed00609a5711fd84d4ba35b494
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-08-13 00:33:34 +1200

    game over logic

--- infinity_race-game/src/main.rs
+++ infinity_race-game/src/main.rs
@@ -31,16 +31,21 @@ fn main() {
         SpritePreset::RacingBarrelBlue,
         SpritePreset::RacingBarrelRed,
         SpritePreset::RacingConeStraight,
-        SpritePreset::RacingConeTurn,
     ];
 
-    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+    for (i, preset) in obstacles_presets.into_iter().enumerate() {
         let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
         obstacle.layer = 5.0;
         obstacle.collision = true;
         obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
         obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
     }
+
+    // create health bar
+  let health_amount = game.add_text("health_amount", "HEALTH: 100");
+  health_amount.translation = Vec2::new(550.0, 320.0);
+
+
 
     game.add_logic(game_logic);
     game.run(GameState {
@@ -52,6 +57,10 @@ fn main() {
 const PLAYER_SPEED: f32 = 250.0;
 const ROAD_SPEED: f32 = 400.00;
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // check if the player lost
+    if game_state.lost {
+        return;
+    }
     //keyboard input
     let mut direction = 0.0;
     if engine.keyboard_state.pressed(KeyCode::Up) {
@@ -78,5 +87,37 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 sprite.translation.x += 1500.0;
             }
         }
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
+        }
+    }
+
+    //handle collisions
+    let health_amount = engine.texts.get_mut("health_amount").unwrap();
+    for event in engine.collision_events.drain(..) {
+        // check if the player collided with an obstacle
+        if !event.pair.either_contains("player1") || event.state.is_end() {
+            continue;
+        }
+        if game_state.health_amount > 0 {
+            game_state.health_amount -= 1;
+            health_amount.value = format!("HEALTH: {}", game_state.health_amount);
+            engine.audio_manager.play_sfx(SfxPreset::Impact3, 0.5);
+        }
+    }
+
+    // check if the game is over
+    if game_state.health_amount == 0 {
+        game_state.lost = true;
+        let game_over = engine.add_text("game over", "GAME OVER");
+        game_over.font_size = 128.0;
+        engine.audio_manager.stop_music();
+        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.5);
     }
 }
+
+
